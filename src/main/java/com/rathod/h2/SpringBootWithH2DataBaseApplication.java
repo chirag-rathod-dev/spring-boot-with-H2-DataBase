ab4f13c6574769dea2d2384e63e579a36787c1d0
chirag-rathod-dev <chirag.rathod.dev@gmail.com>
2025-03-05 16:57:52 +0530

    wip

---
+++ src/main/java/com/rathod/h2/SpringBootWithH2DataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.rathod.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithH2DataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithH2DataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rathod/h2/SpringBootWithH2DataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rathod.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithH2DataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
